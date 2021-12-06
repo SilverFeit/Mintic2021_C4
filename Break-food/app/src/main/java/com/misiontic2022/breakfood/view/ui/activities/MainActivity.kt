93c82c5ed9e533479e4a2516daca8eee7b20c5d4
Daniel_Alvarado <djalvaradog@unal.edu.co>
2021-12-06 00:18:09 -0500

    Break-food 1st commit
    Breakfood

---
+++ Break-food/app/src/main/java/com/misiontic2022/breakfood/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.misiontic2022.breakfood.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.misiontic2022.breakfood.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
